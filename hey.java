08578f75867f53e0a08ff5e72f8093e71062135b
Otabek Yusupov <111352579+otabek20041017@users.noreply.github.com>
2025-01-04 12:36:45 +0500

    Update hey.java

--- hey.java
+++ hey.java
@@ -2,5 +2,9 @@ public class hey {
     public static void main(String[] args) {
         
         System.out.println("Yaxshimisan Otabek Ozbeg and Azizbek Khagan");
+        String firstName = "Otabek ";
+        String lastName = "Ozbeg";
+        System.out.println(firstName + lastName);
+        
     }
 }

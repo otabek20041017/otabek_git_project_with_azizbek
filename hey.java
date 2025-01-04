8fc23974b6b4616241b4566490b8bec7ac023710
Otabek Yusupov <111352579+otabek20041017@users.noreply.github.com>
2025-01-04 13:29:15 +0500

    Changed hey.java with new codes

--- hey.java
+++ hey.java
@@ -1,10 +1,11 @@
 public class hey {
     public static void main(String[] args) {
         
-        System.out.println("Yaxshimisan Otabek Ozbeg and Azizbek Khagan");
-        String firstName = "Otabek ";
-        String lastName = "Ozbeg";
-        System.out.println(firstName + lastName);
+        int age = 20;
+        String name = "Otabek ";
+        String middle_name = "Ozbeg";
+
+        System.out.println("Otabek's age = " + age + "\n" + name + middle_name);
         
     }
 }
